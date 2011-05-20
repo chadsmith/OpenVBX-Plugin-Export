2f4d2e164dbdb0544849b4261ffa98fdb5179f28
Chad Smith <chad@nospam.me>
2011-05-20 01:22:34 -0500

    Fix PHP notices

--- export.php
+++ export.php
@@ -1,38 +1,38 @@
 <?php
 	$user = OpenVBX::getCurrentUser();
 	$tenant_id = $user->values['tenant_id'];
-	if(isset($_POST['id'])){
+	if(isset($_POST['id'])) {
 		$flows = OpenVBX::getFlows(array('id' => $_POST['id'], 'tenant_id' => $tenant_id));
 		$data = $flows[0]->values['data'];
 		$sms_data = $flows[0]->values['sms_data'];
-		$export = md5($data.$sms_data).':'.base64_encode($data).':'.base64_encode($sms_data);
-		if('on' == $_POST['file']){
+		$export = md5($data . $sms_data) . ':' . base64_encode($data) . ':' . base64_encode($sms_data);
+		if(isset($_POST['file'])) {
 			header('Content-type: text/plain');
-			header('Content-Disposition: attachment; filename='.preg_replace('/\W/','',$flows[0]->values['name']).'.ovbx');
+			header('Content-Disposition: attachment; filename=' . preg_replace('/\W/', '', $flows[0]->values['name']) . '.ovbx');
 			echo $export;
-			die();
+			die;
 		}
 	}
 	$flows = OpenVBX::getFlows(array('tenant_id' => $tenant_id));
 ?>
 <style>
-	.vbx-export-flow form{
-		margin-top:20px;
+	.vbx-export-flow form {
+		margin-top: 20px;
 	}
-	.vbx-export-flow p{
-		margin:10px 0;
-		padding:0 20px;
+	.vbx-export-flow p {
+		margin: 10px 0;
+		padding: 0 20px;
 	}
-	.vbx-export-flow h3{
-		font-size:14px;
-		padding:0 20px;
-		margin-top:20px;
+	.vbx-export-flow h3 {
+		font-size: 14px;
+		padding: 0 20px;
+		margin-top: 20px;
 	}
 </style>
 <div class="vbx-content-main">
 	<div class="vbx-content-menu vbx-content-menu-top">
 		<h2 class="vbx-content-heading">Export Flow</h2>
-	</div><!-- .vbx-content-menu -->
+	</div>
     <div class="vbx-table-section vbx-export-flow">
 <?php if(count($flows)): ?>
 		<form method="post" action="">
@@ -41,7 +41,7 @@
 					<label class="field-label">
 						<select name="id" class="medium">
 <?php foreach($flows as $flow): ?>
-							<option value="<?php echo $flow->values['id']; ?>"<?php echo $_POST['id']==$flow->values['id']?' selected="selected"':''; ?>><?php echo $flow->values['name']; ?></option>
+							<option value="<?php echo $flow->values['id']; ?>"<?php echo isset($_POST['id']) && $_POST['id'] == $flow->values['id'] ? ' selected="selected"' : ''; ?>><?php echo $flow->values['name']; ?></option>
 <?php endforeach; ?>
 						</select>
 					</label>
@@ -50,7 +50,7 @@
 				<p><button type="submit" class="submit-button"><span>Export</span></button></p>
 			</fieldset>
 		</form>
-<?php if($export): ?>
+<?php if(isset($export)): ?>
 		<h3>Write this down!</h3>
 		<p><textarea rows="20" cols="100"><?php echo $export; ?></textarea></p>
 <?php endif; ?>
--- import.php
+++ import.php
@@ -1,43 +1,46 @@
 <?php
-	if(isset($_POST['name'])){
-		$name=htmlentities($_POST['name']);
-		if(($file=$_FILES['file']['tmp_name'])&&is_uploaded_file($file))
-			$import=file_get_contents($file);
+	if(!empty($_POST))
+		$error = true;
+	if(!empty($_POST['name'])) {
+		$name = htmlentities($_POST['name']);
+		if(($file = $_FILES['file']['tmp_name']) && is_uploaded_file($file))
+			$import = file_get_contents($file);
 		else
-			$import=$_POST['data'];
-		$parts=explode(':',$import);
-		$data = base64_decode($parts[1]);
-		$sms_data = base64_decode($parts[2]);
-		if(trim($parts[0])==md5($data.$sms_data)){
-			$user = OpenVBX::getCurrentUser();
-			$user_id = $user->values['id'];
-			$tenant_id = $user->values['tenant_id'];
-			$ci =& get_instance();
-			$ci->db->insert('flows', array(
-				'name' => $name,
-				'user_id' => $user_id,
-				'data' => $data,
-				'sms_data' => $sms_data,
-				'tenant_id' => $tenant_id
-			));
+			$import = $_POST['data'];
+		$parts = explode(':', $import);
+		if(count($parts) > 2) {
+			$data = base64_decode($parts[1]);
+			$sms_data = base64_decode($parts[2]);
+			if(trim($parts[0]) == md5($data . $sms_data)) {
+				$user = OpenVBX::getCurrentUser();
+				$user_id = $user->values['id'];
+				$tenant_id = $user->values['tenant_id'];
+				$ci =& get_instance();
+				$ci->db->insert('flows', array(
+					'name' => $name,
+					'user_id' => $user_id,
+					'data' => $data,
+					'sms_data' => $sms_data,
+					'tenant_id' => $tenant_id
+				));
+				$error = false;
+			}
 		}
-		else
-			$error = true;
 	}
 ?>
 <style>
-	.vbx-import-flow form{
-		margin-top:20px;
+	.vbx-import-flow form {
+		margin-top: 20px;
 	}
-	.vbx-import-flow p{
-		margin:10px 0;
-		padding:0 20px;
+	.vbx-import-flow p {
+		margin: 10px 0;
+		padding: 0 20px;
 	}
 </style>
 <div class="vbx-content-main">
 	<div class="vbx-content-menu vbx-content-menu-top">
 		<h2 class="vbx-content-heading">Import Flow</h2>
-	</div><!-- .vbx-content-menu -->
+	</div>
     <div class="vbx-table-section vbx-import-flow">
 		<form method="post" action="" enctype="multipart/form-data">
 			<fieldset class="vbx-input-container">
